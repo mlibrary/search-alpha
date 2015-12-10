60a132d1aafe8244ba0bb12b5e4ec92b45201d06
Earley <earleyj@m-d25q90jsfy13.local>
2015-12-10 12:05:25 -0500

    app.Record view moved into app.Records

--- src/js/app/components/records.js
+++ src/js/app/components/records.js
@@ -5,43 +5,6 @@
 
 var app = app || {}
 
-app.Record = {
-  controller: function(args) {
-    return {
-      getRecordData: function(record) {
-        var data
-        
-        data.renderPartThenCache(function(raw_data) {
-          data = raw_data
-        })
-
-        return data
-      } 
-    }
-  },
-  view: function(ctrl, args) {
-    var record = ctrl.getRecordData(args.record_object)
-
-    console.log('record view')
-    console.log(args)
-
-    return m("li.item", [
-      m("h3", record.names[0]),
-      m("dl",
-        _.reduce(record.fields, function(memo, field) {
-          if ((field.uid != "fullrecord") && (field.uid != "title")) {
-            memo.push(
-              m("dt", field.name),
-              m("dd", field.value)
-            )
-          }
-          return memo
-        }, [])
-      )
-    ])
-  }
-}
-
 app.RecordPlaceholder = {
   view: function() {
     return m("li.item", [
@@ -59,16 +22,11 @@ app.Records = {
     return m("ul.search-items", [
       _.map(app.results(), function(record) {
         if (record) {
-
-          console.log(record)
-
           var data
 
           record.renderPart(function(raw_data) {
             data = raw_data
           })
-
-          console.log(data)
 
           return m("li.item", [
             m("h3", data.names[0]),
